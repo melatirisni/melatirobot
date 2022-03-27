f4b33819b76fa2d1c0c740a8f9d8b39dfb88d3ee
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-03-27 17:45:04 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -18,6 +18,7 @@ force stands for force play.
 • Admins only
 c stands for channel play.
 
+❂ /Settings - Set menu or auth
 ❂ /pause or /cpause - Pause the playing music.
 ❂ /resume or /cresume- Resume the paused music.
 ❂ /mute or /cmute- Mute the playing music

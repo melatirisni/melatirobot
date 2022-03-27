a0806d2bcf44a1b7def526f8608f6dfe6a9db070
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-03-27 17:54:44 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text="About Reyzu Robot", callback_data="emiko_"),
+        InlineKeyboardButton(text="About", callback_data="emiko_"),
     ],
     [
         InlineKeyboardButton(text="Get Help", callback_data="help_back"),
@@ -361,6 +361,7 @@ def emiko_about_callback(update, context):
     if query.data == "emiko_":
         query.message.edit_text(
             text="๏ I'm *Reyzu*, a powerful group management bot built to help you manage your group easily."
+            "\n• I can play music and videos."
             "\n• I can restrict users."
             "\n• I can greet users with customizable welcome messages and even set a group's rules."
             "\n• I have an advanced anti-flood system."

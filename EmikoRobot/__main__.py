d45921376dc0c66a392bc3cae4b81e4311f3b32d
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-03-28 13:50:39 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text="About", callback_data="emiko_"),
+        InlineKeyboardButton(text="Add To Your Group", url=f"t.me/{bu}?startgroup=new"),
     ],
     [
         InlineKeyboardButton(text="Get Help", callback_data="help_back"),
@@ -103,7 +103,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="➗ Add Me To Your Group ➗", url=f"t.me/{bu}?startgroup=new"),
+            text="About", callback_data="emiko_"),
     ],
 ]
 

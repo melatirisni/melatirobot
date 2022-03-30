ad36798c2d429800f855373b9e81767470497de6
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-03-31 00:48:02 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -97,9 +97,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(text="ʜᴇʟᴘ", callback_data="help_back"),
-        InlineKeyboardButton(
-            text="ᴛʀʏ ɪɴʟɪɴᴇ!​​", switch_inline_query_current_chat=""
-        ),
+        InlineKeyboardButton(text="ᴄᴏɴᴛᴀᴄᴛ", url="https://t.me/Reyzuuu"),
     ],
     [
         InlineKeyboardButton(
@@ -383,7 +381,7 @@ def emiko_about_callback(update, context):
                     InlineKeyboardButton(text="ᴄʀᴇᴅɪᴛꜱ", callback_data="emiko_credit"),
                  ],
                  [
-                    InlineKeyboardButton(text="ᴄᴏɴᴛᴀᴄᴛ ʀᴇʏᴢᴜ", url="https://t.me/Reyzuuu"),
+                    InlineKeyboardButton(text="ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​​", url="https://github.com/Reyzuuu/EmikoRobot"),
                  ],
                  [
                     InlineKeyboardButton(text="ɢᴏ ʙᴀᴄᴋ", callback_data="emiko_back"),
@@ -463,10 +461,11 @@ def emiko_about_callback(update, context):
                 [
                  [
                     InlineKeyboardButton(text="Reyzu", url="https://github.com/reyzuuu"),
+                    InlineKeyboardButton(text="Sena-Ex", url="https://github.com/kennedy-ex"),
                  ],
                  [
-                    InlineKeyboardButton(text="Sena-Ex", url="https://github.com/kennedy-ex"),
                     InlineKeyboardButton(text="TheHamkerCat", url="https://github.com/TheHamkerCat"),
+                    InlineKeyboardButton(text="Feri", url="https://github.com/FeriEXP"),
                  ],
                  [
                     InlineKeyboardButton(text="ɢᴏ ʙᴀᴄᴋ", callback_data="emiko_"),

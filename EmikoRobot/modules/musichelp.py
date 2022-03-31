5431dbd67f489acb35dd7e4129d3d60d061965d6
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-03-31 09:51:57 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -8,7 +8,7 @@ c stands for channel play.
 v stands for video play.
 force stands for force play.
 
-❂ /play or /vplay or /cplay (query or reply audio) - - Bot will start playing your given query on voice chat or Stream live links on voice chats.
+❂ /play or /vplay or /cplay (query or reply audio) - Bot will start playing your given query on voice chat or Stream live links on voice chats.
 ❂ /playforce or /vplayforce or /cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
 ❂ /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
 ❂ /playlist - Check Your Saved Playlist On Servers.
@@ -19,10 +19,10 @@ force stands for force play.
 c stands for channel play.
 ❂ /settings - set menu, auth or lang
 ❂ /pause or /cpause - Pause the playing music.
-❂ /resume or /cresume- Resume the paused music.
-❂ /mute or /cmute- Mute the playing music
-❂ /unmute or /cunmute- Unmute the muted music
-❂ /shuffle or /cshuffle- Randomly shuffles the queued playlist.
+❂ /resume or /cresume - Resume the paused music.
+❂ /mute or /cmute - Mute the playing music
+❂ /unmute or /cunmute - Unmute the muted music
+❂ /shuffle or /cshuffle - Randomly shuffles the queued playlist.
 ❂ /seek or /cseek - Forward Seek the music to your duration
 ❂ /seekback or /cseekback - Backward Seek the music to your duration
 ❂ /restart - Restart bot for your chat .

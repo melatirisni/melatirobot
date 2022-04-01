1b4743a540b46bfc87a89566877e222482aa2c7d
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-01 08:51:04 +0700

    Update tagall.py

--- EmikoRobot/modules/tagall.py
+++ EmikoRobot/modules/tagall.py
@@ -109,13 +109,13 @@ async def cancel_spam(event):
         return await event.respond("__Stopped Mention.__")
 
 
-__mod_name__ = "ᴛᴀɢ ᴀʟʟ"
+__mod_name__ = "Tag All"
 __help__ = """
 ──「 Mention all func 」──
 
 Reyzu Can Be a Mention Bot for your group.
 
-Only admins can tag all.  here is a list of commands
+Only admins can tag all.  here is a list of commands.
 
 ❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
 ❂ /cancel for canceling the mention-all.

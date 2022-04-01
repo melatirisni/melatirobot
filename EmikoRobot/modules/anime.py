0c75e42c1a7f7a660173d7bdc87946a368a00cbd
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-01 10:53:41 +0700

    Update anime.py

--- EmikoRobot/modules/anime.py
+++ EmikoRobot/modules/anime.py
@@ -553,20 +553,20 @@ def kayo(update: Update, context: CallbackContext):
 
 __help__ = """
  ──「 Anime search 」──                           
-❂ /anime <anime>: returns information about the anime.
-❂ /whatanime: returns source of anime when replied to photo or gif.                                                          
-❂ /character <character>: returns information about the character.
-❂ /manga <manga>: returns information about the manga.
-❂ /user <user>: returns information about a MyAnimeList user.
+❂ /anime <anime> : returns information about the anime.
+❂ /whatanime : returns source of anime when replied to photo or gif.                                                          
+❂ /character <character> : returns information about the character.
+❂ /manga <manga> : returns information about the manga.
+❂ /user <user> : returns information about a MyAnimeList user.
 ❂ /upcoming: returns a list of new anime in the upcoming seasons.
-❂ /airing <anime>: returns anime airing info.
-❂ /whatanime <anime>: reply to gif or photo.
-❂ /kaizoku <anime>: search an anime on animekaizoku.com
-❂ /kayo <anime>: search an anime on animekayo.com
+❂ /airing <anime> : returns anime airing info.
+❂ /whatanime <anime> : reply to gif or photo.
+❂ /kaizoku <anime> : search an anime on animekaizoku.
+❂ /kayo <anime> : search an anime on animekayo.
 
  「 Anime Quotes 」
-❂ /animequotes: for anime quotes randomly as photos.
-❂ /quote: send quotes randomly as text
+❂ /animequotes : for anime quotes randomly as photos.
+❂ /quote : send quotes randomly as text.
  """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime, run_async=True)
@@ -589,7 +589,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "ᴀɴɪᴍᴇ"
+__mod_name__ = "Anime"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
     "kayo"

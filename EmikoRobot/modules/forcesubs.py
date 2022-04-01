c6783a44162bf2db33d84c49bd986553578e7e0c
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-01 10:13:47 +0700

    Update forcesubs.py

--- EmikoRobot/modules/forcesubs.py
+++ EmikoRobot/modules/forcesubs.py
@@ -184,11 +184,12 @@ __help__ = """
  
 *Commmands*
 ❂ /fsub {channel username} - To turn on and setup the channel.
-  💡Do this first...
+💡 Do this first.
 ❂ /fsub - To get the current settings.
-❂ /fsub disable - To turn of ForceSubscribe..
-  💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
+❂ /fsub disable - To turn of ForceSubscribe.
+💡 If you disable fsub, you need to set again for working.. /fsub {channel username} .
 ❂ /fsub clear - To unmute all members who muted by me.
+
 *Federation*
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
@@ -196,8 +197,8 @@ But then you have many groups, and you don't want this spammer to be in one of y
 You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
 *Commands:*\n
 Feds are now divided into 3 sections for your ease.
-• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-• `/fedadminhelp`*:* Provides help for fed administration commands
-• `/feduserhelp`*:* Provides help for commands anyone can use
+• `/fedownerhelp` *:* Provides help for fed creation and owner only commands.
+• `/fedadminhelp` *:* Provides help for fed administration commands.
+• `/feduserhelp` *:* Provides help for commands anyone can use.
 """
-__mod_name__ = "ꜰ-ꜱᴜʙ/ꜰᴇᴅꜱ"
+__mod_name__ = "F-Sub/Feds"

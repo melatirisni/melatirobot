b49a3f7356a26ca318bed71785a10cf86a9af986
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-01 09:03:23 +0700

    Update reporting.py

--- EmikoRobot/modules/reporting.py
+++ EmikoRobot/modules/reporting.py
@@ -265,12 +265,12 @@ def buttons(update: Update, context: CallbackContext):
 
 
 __help__ = """
-❂ /report <reason>*:* reply to a message to report it to admins.
-❂ @admin*:* reply to a message to report it to admins.
-*NOTE:* Neither of these will get triggered if used by admins.
+❂ /report <reason> *:* reply to a message to report it to admins.
+❂ @admin *:* reply to a message to report it to admins.
+*NOTE :* Neither of these will get triggered if used by admins.
 
-*Admins only:*
-❂ /reports <on/off>*:* change report setting, or view current status.
+*Admins only :*
+❂ /reports <on/off> *:* change report setting, or view current status.
 ❂ If done in pm, toggles your status.
 ❂ If in group, toggles that groups's status.
 """
@@ -291,7 +291,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "ʀᴇᴘᴏʀᴛɪɴɢ"
+__mod_name__ = "Reporting"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),

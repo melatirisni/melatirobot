7cd070169a7da60b75db6cff1a14388976f2534d
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-03 22:52:16 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,16 +1,15 @@
 __mod_name__ = "Music Player"
 
 __help__ = """
-*c* : channel play.
-*v* : video play.
-*force* : force play.
-
-✦ /play or /vplay or /cplay (query or reply audio) : Bot will start playing your given query on voice chat or Stream live links on voice chats.
-✦ /playforce or /vplayforce or /cplayforce : Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-✦ /channelplay [Chat username or id] or [Disable] : Connect channel to a group and stream music on channel's voice chat from your group.
+✦ /play (query) : Play music.
+✦ /vplay (query) : Play video
+✦ /cplay (query) : play on channel
+✦ /channelplay (Chat username or id) or (Disable) : Connect channel to a group and stream music on channel's voice chat from your group.
 ✦ /playlist : Check Your Saved Playlist On Servers.
 ✦ /deleteplaylist : Delete any saved music in your playlist
-✦ /play - Start playing Your Saved Playlist from Servers.
+✦ /play : Start playing Your Saved Playlist from Servers.
+
+*Note :* When playing on the channel make sure the assistant and bot are admins.
 
 *Admins only*
 Come and see [Only For Admins](https://t.me/komunitasvirtual/262)

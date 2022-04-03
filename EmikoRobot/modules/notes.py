18719cd87d835807cd9a14d2eb44ae89731da290
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-03 15:18:15 +0700

    Update notes.py

--- EmikoRobot/modules/notes.py
+++ EmikoRobot/modules/notes.py
@@ -557,22 +557,11 @@ be useful when updating a current note.
 
 *Admins only :*
 ❂ /save <notename> <notedata> *:* saves notedata as a note with name notename.
-A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
-`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info.
 ❂ /save <notename> *:* save the replied message as a note with name notename.
-
-Separate diff replies by `%%%` to get random notes.
-*Example :*
-`/save notename
-Reply 1
-%%%
-Reply 2
-%%%
-Reply 3`
 ❂ /clear <notename> *:* clear note with this name.
 ❂ /removeallnotes *:* removes all notes from the group.
 
- *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
+*Note:* Note names are case-insensitive and they are automatically converted to lowercase before getting saved.
 """
 
 __mod_name__ = "Notes"

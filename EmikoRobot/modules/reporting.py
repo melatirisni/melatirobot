c5e0a229829d3cc1dce8e7308b0b044387613d95
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-03 15:20:38 +0700

    Update reporting.py

--- EmikoRobot/modules/reporting.py
+++ EmikoRobot/modules/reporting.py
@@ -267,6 +267,7 @@ def buttons(update: Update, context: CallbackContext):
 __help__ = """
 ❂ /report <reason> *:* reply to a message to report it to admins.
 ❂ @admin *:* reply to a message to report it to admins.
+
 *NOTE :* Neither of these will get triggered if used by admins.
 
 *Admins only :*

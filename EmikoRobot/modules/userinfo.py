e97145fa869826f85c9b20dc3df1485b946e07ee
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-03 22:36:58 +0700

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -536,31 +536,31 @@ def __user_info__(user_id):
 
 __help__ = """
 *Overall Information about you :*
-❂ /info *:* get information about a user.
-❂ /id *:* get the current group id. If used by replying to a message, gets that user's id.
-❂ /gifid *:* reply to a gif to me to tell you its file ID.
+✦ /info *:* get information about a user.
+✦ /id *:* get the current group id. If used by replying to a message, gets that user's id.
+✦ /gifid *:* reply to a gif to me to tell you its file ID.
  
 *Self addded information :* 
-❂ /setme <text> *:* will set your info.
-❂ /me *:* will get your or another user's info.
+✦ /setme <text> *:* will set your info.
+✦ /me *:* will get your or another user's info.
 Examples :
-❂ /setme I am a wolf.
-❂ /me @username (defaults to yours if no user specified).
+✦ /setme I am a wolf.
+✦ /me @username (defaults to yours if no user specified).
  
-*Information others add on you:* 
-❂ /bio *:* will get your or another user's bio. This cannot be set by yourself.
-❂ /setbio <text> *:* while replying, will save another user's bio .
+*Information others add on you :* 
+✦ /bio *:* will get your or another user's bio. This cannot be set by yourself.
+✦ /setbio <text> *:* while replying, will save another user's bio .
 Examples :
-❂ /bio @username(defaults to yours if not specified).
-❂ /setbio This user is a wolf (reply to the user).
+✦ /bio @username (defaults to yours if not specified).
+✦ /setbio This user is a wolf (reply to the user).
  
-*json Detailed info :*
-❂ /json *:* Get Detailed info about any message.
+*Json Detailed info :*
+✦ /json *:* Get Detailed info about any message.
  
 *AFk :*
 When marked as AFK, any mentions will be replied to with a message stating that you're not available!
-❂ /afk <reason> *:* Mark yourself as AFK.
-  - brb <reason> : Same as the afk command, but not a command. 
+✦ /afk <reason> *:* Mark yourself as AFK.
+✦ /brb <reason> : Same as the afk command, but not a command. 
   
 *What is that health thingy?*
 Come and see [HP System Explained](https://t.me/komunitasvirtual/295)

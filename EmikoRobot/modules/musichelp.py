6b7c086d57da4e1efcfa579e18903d7dbd8eec43
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-04 03:40:10 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,7 +1,7 @@
 __mod_name__ = "Music/Video"
 
 __help__ = """
-✦ /play (query) : Play music, /vplay (query) : Play video, /cplay (query) : play on channel
+✦ /play (query) : Play music, /vplay (query) : Play video, /cplay (query) : play on channel.
 ✦ /channelplay (Chat username or id) or (Disable) : Connect channel to a group and stream music on channel's voice chat from your group.
 ✦ /playlist : Check Your Saved Playlist On Servers.
 ✦ /deleteplaylist : Delete any saved music in your playlist

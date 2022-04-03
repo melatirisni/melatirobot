eb509959449b5d49f7f7c4305b6c0d897d3225d3
Reyzu <102294381+Reyzuuu@users.noreply.github.com>
2022-04-03 18:44:26 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,19 +1,16 @@
 __mod_name__ = "Music Player"
 
 __help__ = """
-*Available Commands : play, vplay, cplay.*
-*ForcePlay Commands : playforce, vplayforce, cplayforce.*
+*c* : channel play.
+*v* : video play.
+*force* : force play.
 
-*c stands for channel play.*
-*v stands for video play.*
-*force stands for force play.*
-
-❂ /play or /vplay or /cplay (query or reply audio) - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-❂ /playforce or /vplayforce or /cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-❂ /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
-❂ /playlist - Check Your Saved Playlist On Servers.
-❂ /deleteplaylist - Delete any saved music in your playlist
-❂ /play  - Start playing Your Saved Playlist from Servers.
+✦ /play or /vplay or /cplay (query or reply audio) : Bot will start playing your given query on voice chat or Stream live links on voice chats.
+✦ /playforce or /vplayforce or /cplayforce : Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+✦ /channelplay [Chat username or id] or [Disable] : Connect channel to a group and stream music on channel's voice chat from your group.
+✦ /playlist : Check Your Saved Playlist On Servers.
+✦ /deleteplaylist : Delete any saved music in your playlist
+✦ /play - Start playing Your Saved Playlist from Servers.
 
 *Admins only*
 Come and see [Only For Admins](https://t.me/komunitasvirtual/262)
